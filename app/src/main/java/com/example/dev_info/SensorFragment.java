db009aaea01de972e3e25feaad5d164e5f624178
Arpit Chaurasia <benchaurasia@gmail.com>
2023-04-28 02:55:46 +0530

    init: sensors

--- app/src/main/java/com/example/dev_info/SensorFragment.java
+++ app/src/main/java/com/example/dev_info/SensorFragment.java
@@ -1,5 +1,11 @@
 package com.example.dev_info;
 
+import static android.content.Context.SENSOR_SERVICE;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +19,7 @@ import android.widget.TextView;
 
 public class SensorFragment extends Fragment {
 
+    TextView gpstxt, gyrotxt, barotxt, accelerotxt, rotvectxt, proxtxt, ambitxt;
     public SensorFragment() {
     }
 
@@ -27,6 +34,111 @@ public class SensorFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        gpstxt = view.findViewById(R.id.tv_gps);
+        gyrotxt = view.findViewById(R.id.tv_gyro);
+        barotxt = view.findViewById(R.id.tv_baro);
+        accelerotxt = view.findViewById(R.id.tv_accelero);
+        rotvectxt = view.findViewById(R.id.tv_rotvec);
+        proxtxt = view.findViewById(R.id.tv_prox);
+        ambitxt = view.findViewById(R.id.tv_ambi_light);
 
+        SensorManager sm = (SensorManager) getContext().getSystemService(SENSOR_SERVICE);
+
+        //GPS, Gyroscope, Baro, Accelero, Rotation Vector, Proximity, Ambient light
+
+        Sensor gps = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor gyro = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        Sensor baro = sm.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        Sensor accel = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor rot_vec = sm.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        Sensor prox = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        Sensor ambi = sm.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+
+        SensorEventListener gyroListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                gyrotxt.setText("Gyrometer = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        SensorEventListener baroListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                barotxt.setText("Barometer = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        SensorEventListener acceleroListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                accelerotxt.setText("Accelerometer = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        SensorEventListener rotvecListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                rotvectxt.setText("Rotation Vector = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        SensorEventListener proxListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                proxtxt.setText("Proximity = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        SensorEventListener ambiListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                ambitxt.setText("Ambient Light = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+        SensorEventListener gpsListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                gpstxt.setText("GPS = " + event.values[0]);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sm.registerListener(gpsListener, gps, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(gyroListener, gyro, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(baroListener, baro, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(acceleroListener, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(rotvecListener, rot_vec, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(proxListener, prox, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(ambiListener, ambi, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
